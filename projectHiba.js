50d5592e8beddb7c29f87e124a8d8a3045a12da3
Mei <wangchunmei919@aliyun.com>
2019-09-18 15:56:39 +0200

    Initial commit

---
+++ projectHiba.js
@@ -0,0 +1,8 @@
+function onFunction(){
+    document.getElementById("colorImportant").style.color ="red";
+
+}
+
+function offFunction(){
+    document.getElementById("colorImportant").style.color="black";
+}
